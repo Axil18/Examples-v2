51730e1d5baede20b614b7c5de5f82d0f58d90f5
Aleksandr Elovikov <aleksandrelovikov@mail.ru>
2023-02-09 22:02:05 +0100

    IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Hello Amigos Mashka");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
