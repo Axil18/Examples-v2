bd3e147df8e336840d30fd4e28b9b0d767600cb6
Aleksandr Elovikov <aleksandrelovikov@mail.ru>
2023-03-04 13:36:19 +0100

    lekciya3

---
+++ Example012_Methods/Program.cs
@@ -0,0 +1,143 @@
+﻿// //Вид 1
+// // void Method1()
+// // {
+// //     Console.WriteLine("Axil18");
+// // }
+// // Method1();
+
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+// //Method2("hi jack");
+
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+// //Method21("hello", 5);
+// Method21(msg: "Text", count: 5);
+// Method21(count: 5, msg: "Text");
+
+// Вид 3
+
+// int Method3()
+// {
+//     return DateTime.Now.Month;
+// }
+// int month = Method3();
+// Console.WriteLine(month);
+
+// //Вид 4
+
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = string.Empty;
+//     while(i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "text");          // Тоже самое делает Console.WriteLine(Method4(10, "text"));
+// Console.WriteLine(res);
+
+
+// string Method4(int count, string text)
+// {
+
+//     string result = string.Empty;
+//     for (int i = 0; i < count; i++)
+//     {
+//         result = result + text;
+//     }
+//     return result;
+// }
+// // string res = Method4(10, "z");
+// // Console.WriteLine(res);
+
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i * j}");
+//     }
+//     Console.WriteLine();
+// }
+
+//Итак, задача. Дан текст. В нём нужно все пробелы заменить чёрточками, маленькие буквы «к»
+//заменить большими «К», а большие «С» заменить на маленькие «с».
+string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+// string s = “qwerty”
+//              012345
+// s[3] // r
+
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = String.Empty;
+
+//     int length = text.Length;
+//     for (int i = 0; i < length; i++)
+//     {
+//         if (text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+//     return result;
+// }
+// string newText = Replace(text, ' ', '|');//замена пробелов|
+
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'к', 'К');
+// Console.WriteLine(newText);
+// newText = Replace(newText, 'C', 'c');
+// Console.WriteLine(newText);
+
+// 1. Найти позицию минимального элемента в неотсортированной части массива.
+// 2. Произвести обмен этого значения со значением первой неотсортированной позиции.
+// 3. Повторять пока есть неотсортированные элементы.
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+void SellectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+
+    }
+}
+    
+PrintArray(arr);
+SellectionSort(arr);
+
+PrintArray(arr);
